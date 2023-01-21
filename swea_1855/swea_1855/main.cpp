9efb705a48a5d05d11e4a3e6f5a133ad043f977b
wonchul98 <89337820+wonchul98@users.noreply.github.com>
2023-01-21 18:07:27 +0900

    commit for 1855

--- swea_1855/swea_1855/main.cpp
+++ swea_1855/swea_1855/main.cpp
@@ -2,15 +2,16 @@
 
 #include<iostream>
 #include<queue>
+#include<cmath>
 
 using namespace std;
 
-int parent[100001];
-int depth[100001];
-int dp[100000][17];
+int parent[100002];
+int depth[100002];
+int dp[100002][16];
 int N;
 long long cnt = 0;
-queue<int> child[100001];
+queue<int> child[100002];
 queue<int> Q;
 
 void solve();
@@ -19,7 +20,7 @@ int main(int argc, char** argv)
 {
 	int test_case;
 	int T;
-	freopen("input.txt", "r", stdin);
+	freopen("input2.txt", "r", stdin);
 	cin >> T;
 	for (test_case = 1; test_case <= T; ++test_case)
 	{
@@ -27,18 +28,14 @@ int main(int argc, char** argv)
 		cin >> N;
 		int input;
 		depth[1] = 0;
-		
 		for (int i = 2; i <= N; i++) {
 			cin >> input;
 			parent[i] = input;
 			child[input].push(i);
 			depth[i] = depth[input] + 1;
-			if (i == 2) dp[i][depth[i] - 1] = input;
-			else {
-				for (int j = 0; j < depth[i] - 1; j++) {
-					dp[i][j] = dp[input][j];//부모의 배열 가져옴
-					dp[i][depth[i] - 1] = input;//부모
-				}
+			dp[i][0] = input;//0번에는 부모 저장
+			for (int j = 1; j < floor(log2(depth[i]))+1; j++) {//1번 부터는 dp
+				dp[i][j] = dp[dp[i][j - 1]][j - 1];
 			}
 		}
 		solve();
@@ -62,21 +59,30 @@ void solve() {
 		/*연산*/
 		if (Q.empty()) break;
 		cur = Q.front();
+		bool flag = false;
+		//cout << "cur: " << cur << " front: " << front << endl;
 		if (depth[cur] != depth[front]) { //층이 다른경우
-			int save_cur = cur;
-			while (parent[cur] != front) {
-				cur = parent[cur];
-				front = parent[front];
-			}
-			cnt += (2 * (depth[save_cur] - depth[cur])) + 1;
+			cur = parent[cur]; //층을 맞춰줌
+			flag = true;
 		}
-		else {//층이 같은 경우
-			int save_cur = cur;
-			while (cur != front) {
-				cur = parent[cur];
-				front = parent[front];
+		int save_cur = cur; //저장
+		int save_front = front;
+		int iter = 0;
+		while (cur != front) {
+			cur = dp[save_cur][iter];
+			front = dp[save_front][iter];
+			//cout << "cur: " << cur << " front: " << front << " iter: " << iter << endl;
+			iter += 1;
+			if (cur == front && pow(2, iter-1) <= depth[cur]) {
+				if (iter == 1) break;
+				cur = dp[cur][iter - 1];
+				front = dp[front][iter - 1];
+				iter = 0;
 			}
-			cnt += 2 * (depth[save_cur] - depth[cur]);
 		}
+		//cout << "공통 조상: " << cur << endl;
+		if(flag) cnt += (2 * (depth[save_cur] - depth[cur])) + 1;
+		else cnt += (2 * (depth[save_cur] - depth[cur]));
+		//cout << "cnt += " << (2 * (depth[save_cur] - depth[cur])) + 1 << " cnt: " << cnt << endl;
 	}
 }
